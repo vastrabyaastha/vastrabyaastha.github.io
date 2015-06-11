f005e51d6cb7fb62acb4ab70a1f9c8b29665a952
Exsete <connect@exsete.com>
2015-06-11 17:18:29 +0530

    mobile rendering 1.0

--- data/js/height.js
+++ data/js/height.js
@@ -1,6 +1,17 @@
-winheight=$(window).height();
+var document;
+var window;
+var $;
+var winheight=$(window).height();
+var divHeight=(winheight-$("#background-img").height()-$("#top_nav_div").height())/3;
 $(document).ready(function(){
     $('#home-page').css('max-height',winheight-17 + 'px');
+    //for mobile site
+    if($(window).width()<768){
+        $('#home-page').css('height',winheight-17 + 'px');
+        $("#section-1,#section-2,#section-3,#section-sub-1,#section-sub-2,#section-sub-3").css("height",divHeight);
+        $("#section-2").css("bottom",divHeight);
+        $("#section-3").css("bottom","0");
+    }
     $('#section-1,#section-2,#section-3,divider-div-1,divider-div-2').css('max-height',winheight-50 + 'px');
     $('#contact-page-div').css('height',winheight-61 + 'px');
     $('.cont-bottom-txt').css('bottom',-winheight+59+'px');
