192dad8e0d70eca4dee69c2aaa88e9bfca3d77fe
Exsete <connect@exsete.com>
2015-06-13 18:02:05 +0530

    testing mobile 1.0

--- data/js/height.js
+++ data/js/height.js
@@ -16,4 +16,19 @@ $(document).ready(function(){
     $('#section-1,#section-2,#section-3,divider-div-1,divider-div-2').css('max-height',winheight-50 + 'px');
     $('#contact-page-div').css('height',winheight-61 + 'px');
     $('.cont-bottom-txt').css('bottom',-winheight+59+'px');
+    $(window).resize(function(){
+        //$('div').css("border","5px solid red");
+        $('#home-page').css('max-height',winheight-17 + 'px');
+        //for mobile site
+        if($(window).width()<=768){
+            $('#home-page').css('height',winheight-17 + 'px');
+            $("#section-1,#section-2,#section-3,#section-sub-1,#section-sub-2,#section-sub-3").css("height",divHeight);
+            $("#section-1").css("bottom",(2*divHeight)+1);
+            $("#section-2").css("bottom",divHeight);
+            $("#section-3").css("bottom","0");
+        }
+        $('#section-1,#section-2,#section-3,divider-div-1,divider-div-2').css('max-height',winheight-50 + 'px');
+        $('#contact-page-div').css('height',winheight-61 + 'px');
+        $('.cont-bottom-txt').css('bottom',-winheight+59+'px');
+    });
 });
