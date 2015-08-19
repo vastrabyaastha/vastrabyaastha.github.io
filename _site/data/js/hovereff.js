122ef2a9faf493d6de7190571c8b894400618e23
Exsete <connect@exsete.com>
2015-08-19 14:04:03 +0530

    img changed to sketches

--- _site/data/js/hovereff.js
+++ _site/data/js/hovereff.js
@@ -1,4 +1,5 @@
 $('document').ready(function(){
+    //for making the slide up effect for top links
     if($(window).width()>480){
         $("#section-1").hover(function(){
             $("#section-2,#section-3").addClass("hover-background-l");
@@ -30,27 +31,4 @@ $('document').ready(function(){
             $("#section-sub-3").css("height","59px");
         });
     }
-    /*if($(window).width()<=480){
-        $("#section-1").hover(function(){
-            $("#section-1").css("height","200px");
-        },function(){
-            $("#section-sub-1").css("height","102px");
-            $("#section-1").css("height","102px");
-        });
-        $("#section-2").hover(function(){
-            $("#section-2").css("height","200px");
-        },function(){
-            $("#section-sub-2").css("height","102px");
-            $("#section-2").css("height","102px");
-        });
-        $("#section-3").hover(function(){
-            $("#section-2,#section-1,#section-sub-2,#section-sub-1").css("height","93px");
-            $("#section-3").css("height","211px");
-            $("#section-sub-3").css("height","222px");
-        },function(){
-            $("#section-2,#section-1,#section-sub-2,#section-sub-1").css("height","102px");
-            $("#section-sub-3").css("height","102px");
-            $("#section-3").css("height","102px");
-        });
-    }*/
 });
